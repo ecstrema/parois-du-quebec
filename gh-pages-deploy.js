f38f3ffd84edd942451c1299802fd5ef72c18bca
Remi Marche <remimarche@gmail.com>
2022-02-10 12:17:52 -0500

    verify that the current branch is master before deploying

--- gh-pages-deploy.js
+++ gh-pages-deploy.js
@@ -1,5 +1,25 @@
 import { publish } from 'gh-pages';
 
+import { exec } from 'child_process';
+import { exit } from 'process';
+
+const getBranch = () => new Promise((resolve, reject) => {
+    return exec('git rev-parse --abbrev-ref HEAD', (err, stdout, stderr) => {
+        if (err)
+            reject(`getBranch Error: ${err}`);
+        else if (typeof stdout === 'string')
+            resolve(stdout.trim());
+    });
+});
+
+if (await getBranch() !== "master") {
+    console.log(await getBranch());
+    console.error("fatal: currently not on branch master");
+    exit(1);
+}
+
+console.log("Deploying...")
+
 publish(
     'build',
     {
