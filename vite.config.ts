5b0eef49b55530d43ebc9126ef7f175fe86e3d10
Remi Marche <remimarche@gmail.com>
2024-07-04 10:35:26 +0200

    fix svg

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import { enhancedImages } from '@sveltejs/enhanced-img';
+import svg from '@poppanator/sveltekit-svg'
 
 
 export default defineConfig({
 	plugins: [
 		enhancedImages(),
 		sveltekit(),
+		svg(),
 	],
 });
