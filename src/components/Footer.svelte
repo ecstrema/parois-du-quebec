48792132c6b46cb4237c72799411de0c5fced6cb
Remi Marche <remimarche@gmail.com>
2024-07-04 10:36:11 +0200

    Revert "fix svg"
    This reverts commit 5b0eef49b55530d43ebc9126ef7f175fe86e3d10.

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,13 +1,20 @@
 <script>
-    import Logo from './logo-only.svg';
-    import GitHub from './GitHub.svg';
+    import Logo from './logo.svg';
 </script>
 <footer>
     <div class="copyright">
-        <Logo width="60px" height="90px" style="margin-right: 20px;"/>
+        <img src={Logo} alt="logo" width="80px" style="margin-right: 20px;">
         <p>Marche & Cornellier © {new Date().getFullYear()}</p>
         <a class="gh" href="https://github.com/marr11317/escalade-laurentides" target="_blank" rel="noopener noreferrer">
-            <GitHub width="1.3em" height="1.3em" />
+            <svg width="1.3em" height="1.3em" viewBox="0 0 24 24">
+                <path
+                    fill="none"
+                    stroke-width="1.5"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M9 19c-4.3 1.4-4.3-2.5-6-3m12 5v-3.5c0-1 .1-1.4-.5-2c2.8-.3 5.5-1.4 5.5-6a4.6 4.6 0 0 0-1.3-3.2a4.2 4.2 0 0 0-.1-3.2s-1.1-.3-3.5 1.3a12.3 12.3 0 0 0-6.2 0C6.5 2.8 5.4 3.1 5.4 3.1a4.2 4.2 0 0 0-.1 3.2A4.6 4.6 0 0 0 4 9.5c0 4.6 2.7 5.7 5.5 6c-.6.6-.6 1.2-.5 2V21"
+                />
+            </svg>
         </a>
     </div>
 </footer>
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,11 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import { enhancedImages } from '@sveltejs/enhanced-img';
-import svg from '@poppanator/sveltekit-svg'
 
 
 export default defineConfig({
 	plugins: [
 		enhancedImages(),
 		sveltekit(),
-		svg(),
 	],
 });
